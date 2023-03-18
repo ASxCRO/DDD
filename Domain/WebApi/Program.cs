e8aa7593a2658e504443e694070e85e463d96035
ASxCRO <antonio.suups@gmail.com>
2023-03-18 20:42:43 +0100

    added serilog

--- Domain/WebApi/Program.cs
+++ Domain/WebApi/Program.cs
@@ -1,24 +1,30 @@
 ﻿using Application;
-using Infrastructure;
+//using Infrastructure;
 using Presentation;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services
-    .AddApplication()
-    .AddInfrastructure()
-    .AddPresentation();
+//builder.Services
+//    .AddApplication()
+//    .AddInfrastructure()
+//    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration);
 
 var app = builder.Build();
 
-if (app.Enviwronment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
